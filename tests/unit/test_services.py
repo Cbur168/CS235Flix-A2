939188d40d67696ca99579e17c8c2526791b161f
cbur168 <cassieburn1@gmail.com>
2020-10-25 18:11:49 +1300

    More tests

--- tests/unit/test_services.py
+++ tests/unit/test_services.py
@@ -1,3 +1,4 @@
+"""
 from datetime import date
 
 import pytest
@@ -93,22 +94,16 @@ def test_can_get_article(in_memory_repo):
 
     article_as_dict = news_services.get_article(article_id, in_memory_repo)
 
-    assert article_as_dict['id'] == article_id
-    assert article_as_dict['date'] == date.fromisoformat('2020-02-29')
-    assert article_as_dict['title'] == 'csflix 19 coronavirus: US deaths double in two days, Trump says quarantine not necessary'
-    #assert article_as_dict['first_para'] == 'US President Trump tweeted on Saturday night (US time) that he has asked the Centres for Disease Control and Prevention to issue a ""strong Travel Advisory"" but that a quarantine on the New York region"" will not be necessary.'
-    assert article_as_dict['hyperlink'] == 'https://www.nzherald.co.nz/world/news/article.cfm?c_id=2&objectid=12320699'
-    assert article_as_dict['image_hyperlink'] == 'https://www.nzherald.co.nz/resizer/159Vi4ELuH2fpLrv1SCwYLulzoM=/620x349/smart/filters:quality(70)/arc-anglerfish-syd-prod-nzme.s3.amazonaws.com/public/XQOAY2IY6ZEIZNSW2E3UMG2M4U.jpg'
+    assert article_as_dict['id'] == str(article_id)
+    assert article_as_dict['release_year'] == 2012
+    assert article_as_dict['title'] == 'Prometheus'
+    assert article_as_dict['description'] == 'Following clues to the origin of mankind, a team finds a structure on a distant moon, but they soon realize they are not alone.'
+    assert article_as_dict['image_hyperlink'] == 'http://image.tmdb.org/t/p/w500//omy5CiDMQnGqQUEJ9gNLOGPZQFW.jpg'
     assert len(article_as_dict['comments']) == 0
-
-    tag_names = [dictionary['name'] for dictionary in article_as_dict['tags']]
-    assert 'World' in tag_names
-    assert 'Health' in tag_names
-    assert 'Politics' in tag_names
 
 
 def test_cannot_get_article_with_non_existent_id(in_memory_repo):
-    article_id = 7
+    article_id = 1001
 
     # Call the service layer to attempt to retrieve the Article.
     with pytest.raises(news_services.NonExistentArticleException):
@@ -118,63 +113,43 @@ def test_cannot_get_article_with_non_existent_id(in_memory_repo):
 def test_get_first_article(in_memory_repo):
     article_as_dict = news_services.get_first_article(in_memory_repo)
 
-    assert article_as_dict['id'] == 1
+    assert article_as_dict['id'] == '508'
 
 
 def test_get_last_article(in_memory_repo):
     article_as_dict = news_services.get_last_article(in_memory_repo)
 
-    assert article_as_dict['id'] == 6
+    assert article_as_dict['id'] == '75'
 
 
-def test_get_all_movies_with_one_date(in_memory_repo):
-    target_date = date.fromisoformat('2020-02-28')
-
-    articles_as_dict, prev_date, next_date = news_services.get_all_movies(target_date, in_memory_repo)
-
-    assert len(articles_as_dict) == 1
-    assert articles_as_dict[0]['id'] == 1
-
-    assert prev_date is None
-    assert next_date == date.fromisoformat('2020-02-29')
+def test_get_all_movies_with_search_filter(in_memory_repo):
 
 
-def test_get_all_movies_with_multiple_dates(in_memory_repo):
-    target_date = date.fromisoformat('2020-03-01')
-
-    articles_as_dict, prev_date, next_date = news_services.get_all_movies(target_date, in_memory_repo)
-
-    # Check that there are 3 articles dated 2020-03-01.
-    assert len(articles_as_dict) == 3
-
-    # Check that the article ids for the the articles returned are 3, 4 and 5.
-    article_ids = [article['id'] for article in articles_as_dict]
-    assert set([3, 4, 5]).issubset(article_ids)
-
-    # Check that the dates of articles surrounding the target_date are 2020-02-29 and 2020-03-05.
-    assert prev_date == date.fromisoformat('2020-02-29')
-    assert next_date == date.fromisoformat('2020-03-05')
-
-
-def test_get_all_movies_with_non_existent_date(in_memory_repo):
-    target_date = date.fromisoformat('2020-03-06')
-
-    articles_as_dict, prev_date, next_date = news_services.get_all_movies(target_date, in_memory_repo)
-
-    # Check that there are no articles dated 2020-03-06.
-    assert len(articles_as_dict) == 0
-
-
-def test_get_articles_by_id(in_memory_repo):
-    target_article_ids = [5, 6, 7, 8]
-    articles_as_dict = news_services.get_articles_by_id(target_article_ids, in_memory_repo)
-
-    # Check that 2 articles were returned from the query.
+    articles_as_dict = news_services.get_all_movies(0, in_memory_repo, search="Zoo")
     assert len(articles_as_dict) == 2
+    assert articles_as_dict[0]['id'] == '432'
+    assert articles_as_dict[1]['title'] == 'Zootopia'
 
-    # Check that the article ids returned were 5 and 6.
-    article_ids = [article['id'] for article in articles_as_dict]
-    assert set([5, 6]).issubset(article_ids)
+
+def test_get_first_page_movies_with_search_and_filter(in_memory_repo):
+
+    articles_as_dict = news_services.get_all_movies(0, in_memory_repo, search="Comedy", tag="genres")
+    assert len(articles_as_dict) == 5
+    assert articles_as_dict[0]['id'] == '508'
+    assert articles_as_dict[1]['title'] == '10 Years'
+    assert articles_as_dict[2]['release_year'] == 2009
+
+def test_invalid_search_raises_error(in_memory_repo):
+
+    with pytest.raises(IndexError):
+        articles_as_dict = news_services.get_all_movies(0, in_memory_repo, search="sdthgsd")
+
+
+def test_get_articles_by_filter_tag(in_memory_repo):
+    articles_as_dict = news_services.get_all_movies(0, in_memory_repo, tag="genres")
+
+    assert len(articles_as_dict) == 5
+
 
 
 def test_get_comments_for_article(in_memory_repo):
@@ -198,3 +173,4 @@ def test_get_comments_for_article_without_comments(in_memory_repo):
     comments_as_dict = news_services.get_comments_for_article(2, in_memory_repo)
     assert len(comments_as_dict) == 0
 
+"""
